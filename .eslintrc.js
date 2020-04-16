24fb36a14ae2cfc9f2daa1cdb4a8af35aa6179f1
ShotaArai29 <shoarai@pasona.tech>
2020-04-16 12:41:21 +0900

    eslintを追加

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // TypeScript: @cybozu/eslint-config/presets/typescript
+    // react + TypeScript: @cybozu/eslint-config/presets/react-typescript
+    extends: "@cybozu/eslint-config",
+    env: {
+        browser: true,
+    },
+    rules: {
+        // デフォルトは2. 自由に設定して良い
+        indent: ["error", 4],
+    },
+    globals: {
+        kintone: "readonly",
+        // CDN にライブラリを追加したら追記
+        // "moment" : "readonly"
+        // "jQuery" : "readonly"
+    },
+};
