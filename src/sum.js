2521b1e9211ce2dbc4b77c580d4b5ede0e66e6d8
ShotaArai29 <shoarai@pasona.tech>
2020-06-30 13:11:43 +0900

    add argChange testCase

--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,12 @@
 export const sum = (a, b) => a + b;
+
+export const argChange = (arg) => {
+    switch (arg) {
+        case "A":
+            return "商品コード123";
+        case "B":
+            return "商品コード222";
+        default:
+            return "エラーE001";
+    }
+};
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,15 @@
-import { sum } from "./sum";
+import { sum, argChange } from "./sum";
 
 test("adds 1 + 2 to equal 3", () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+test("引数を商品コードに変換", () => {
+    const tests = [
+        { input: "A", expected: "商品コード123" },
+        { input: "B", expected: "商品コード222" },
+    ];
+    for (const t of tests) {
+        expect(argChange(t.input)).toBe(t.expected);
+    }
 });
